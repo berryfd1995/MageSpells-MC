8d25d0b15605f0740db38332af06dad522a5f619
Daniel Berry <berryfd1995@gmail.com>
2016-09-20 16:54:10 -0400

    GUI addition

--- src/me/L2_Envy/MSRM/Alchemy/GUI/BrewingMenu.java
+++ src/me/L2_Envy/MSRM/Alchemy/GUI/BrewingMenu.java
@@ -6,6 +6,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 
@@ -16,6 +17,35 @@ public class BrewingMenu {
     private AlchemyManager alchemyManager;
     private MageSpellsManager mageSpellsManager;
     private ArrayList<String> playerinbrewingmenu;
+    /*
+      0 = AIR
+      1 = Black Glass
+      2 = Item
+      3 = Result
+     */
+    private static final int[][] layout =
+                   {{0,0,0,0,2,0,0,0,0},
+                    {0,0,0,1,1,1,0,0,0},
+                    {0,2,1,1,3,1,1,2,0},
+                    {0,0,1,1,1,1,1,0,0},
+                    {0,0,1,1,0,1,1,0,0},
+                    {0,2,1,0,0,0,1,2,0}};
+    /**
+     * [00][01][02][03][04][05][06][07][08]
+     * [09][10][11][12][13][14][15[16][17]
+     * [18][19][20][21][22][23][24][25][26]
+     * [27][28][29][30][31][32][33][34][35]
+     * [36][37][38][39][40][41][42][43][44]
+     * [45][46][47][48][49][50][51][52][53]
+     **/
+    /**
+     * [*][*][*][*][I][*][*][*][*]
+     * [*][*][*][I][I][I][*][*][*]
+     * [I][I][I][I][I][I][I][I][I]
+     * [*][I][I][I][I][I][I][I][*]
+     * [*][*][I][I][I][I][I][*][*]
+     * [*][I][I][*][*][*][I][I][*]
+     */
     public BrewingMenu(){
         playerinbrewingmenu = new ArrayList<>();
     }
@@ -38,24 +68,30 @@ public class BrewingMenu {
         return playerinbrewingmenu.contains(player.getName());
     }
     private Inventory loadInventory(){
-        /**
-         * [00][01][02][03][04][05][06][07][08]
-         * [09][10][11][12][13][14][15[16][17]
-         * [18][19][20][21][22][23][24][25][26]
-         * [27][28][29][30][31][32][33][34][35]
-         * [36][37][38][39][40][41][42][43][44]
-         * [45][46][47][48][49][50][51][52][53]
-         **/
-        /**
-         * [*][*][*][*][*][*][*][*][*]
-         * [*][*][ ][ ][ ][ ][ ][*][*]
-         * [*][ ][ ][ ][ ][ ][ ][ ][*]
-         * [*][ ][ ][ ][ ][ ][ ][ ][*]
-         * [*][*][ ][ ][ ][ ][ ][*][*]
-         * [*][*][*][*][*][*][*][*][*]
-         **/
+        ItemStack[] contents = new ItemStack[54];
+
+        for(int i = 0; i <6; i++){
+            for(int j = 0; j < 9; j++){
+                int s = (i*10) + j;
+                switch (layout[i][j]){
+                    case 0:
+                        contents[s] = alchemyManager.main.utils.getItemStack("AIR");
+                        break;
+                    case 1:
+                        contents[s] = alchemyManager.main.utils.getItemStack("STAINED_GLASS_PANE-15");
+                        break;
+                    case 2:
+                        contents[s] = alchemyManager.main.utils.getItemStack("STAINED_GLASS_PANE-13", "&cIngrediant");
+                        break;
+                    case 3:
+                        contents[s] = alchemyManager.main.utils.getItemStack("GLASS_BOTTLE", "&cResult");
+                        break;
+                }
+            }
+        }
         Inventory inv = Bukkit.createInventory(null,54, ChatColor.DARK_PURPLE +"Brewing Menu");
-        inv.setItem(53, mageSpellsManager.main.utils.getItemStack("STAINED_GLASS_PANE-13", ChatColor.GREEN +"Create Potion"));
+        inv.setContents(contents);
+        //inv.setItem(53, mageSpellsManager.main.utils.getItemStack("STAINED_GLASS_PANE-13", ChatColor.GREEN +"Create Potion"));
         return inv;
     }
 }
